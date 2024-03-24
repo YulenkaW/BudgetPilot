c5055d86b7a3959895c5146e14fc6ec09e08fcf2
Yuliia Wickens <83892913+YulenkaW@users.noreply.github.com>
2024-03-24 00:00:25 -0400

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import BudgetInput from './components/BudgetInput';
+import AddExpense from './components/AddExpense.jsx';
+import ExpenseList from './components/ExpenseList.jsx';
+import { ExpensesProvider } from './components/ExpensesContext.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* Render the BudgetInput */}
+        <BudgetInput />
+        <ExpensesProvider>
+          <h1>Enter Expenses</h1>
+          <AddExpense />
+          <ExpenseList />
+        </ExpensesProvider>
+      </header>
+    </div>
+  );
+}
+
+export default App;
