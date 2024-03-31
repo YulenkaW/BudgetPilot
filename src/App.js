05224376bd5c7e9023b4440d900cbca381d5504c
DESKTOP-54V97R8\akade <qkader03@gmail.com>
2024-03-31 18:46:52 -0400

    added financial tips and asjusted app home screen. created pages

--- src/App.js
+++ src/App.js
@@ -1,37 +1,53 @@
-// App.js
 import React, { useState } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import BudgetInput from './components/BudgetInput';
 import AddExpense from './components/AddExpense.jsx';
 import ExpenseList from './components/ExpenseList.jsx';
 import { ExpensesProvider } from './components/ExpensesContext.jsx';
-import Login from './components/Login.jsx'; 
+import Login from './components/Login.jsx';
+import FinancialTipPage from './components/FinancialTipPage.jsx';
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
 
   const handleLogin = () => {
-    // For simplicity, just set loggedIn state to true
     setLoggedIn(true);
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        {!loggedIn ? (
-          <Login onLogin={handleLogin} />
-        ) : (
-          <>
-            <BudgetInput />
-            <ExpensesProvider>
-              <h1>Enter Expenses</h1>
-              <AddExpense />
-              <ExpenseList />
-            </ExpensesProvider>
-          </>
-        )}
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          {!loggedIn ? (
+            <Login onLogin={handleLogin} />
+          ) : (
+            <>
+              <h1>BudgetPilot</h1>
+              <div className="button-container">
+                <Link to="/budget-input">
+                  <button>Budget Input</button>
+                </Link>
+                <Link to="/add-expense">
+                  <button>Add Expense</button>
+                </Link>
+                <Link to="/financial-tip">
+                  <button>Financial Tips</button>
+                </Link>
+              </div>
+              <Routes>
+                <Route path="/budget-input" element={<BudgetInput />} />
+                <Route path="/add-expense" element={<AddExpense />} />
+                <Route path="/financial-tip" element={<FinancialTipPage />} />
+              </Routes>
+              <ExpensesProvider>
+                <ExpenseList />
+              </ExpensesProvider>
+            </>
+          )}
+        </header>
+      </div>
+    </Router>
   );
 }
 
